8149f185167b4c3e6d6b3afac93df0439fc1045e
kelocoes <kevin.rodriguez109@gmail.com>
2026-07-24 17:07:29 -0500

    refactor: migrate simple Java web server to a standard Maven-based Jakarta EE project structure

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("""
+                   <head>
+                       <title>Hello Servlet</title>
+                       <link rel="stylesheet" type="text/css" href="css/main.css">
+                       <script src="js/main.js"></script>
+                   </head>
+                """);
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+        System.out.println("HelloServlet destroyed");
+    }
+}
